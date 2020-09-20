50d46cc71761cf8bc4b1b7e687730f82c52e2872
Krishtof Korda <krishtofkorda@gmail.com>
2020-09-20 15:34:16 -0700

    Remove nvidia comments

--- install-docker.sh
+++ install-docker.sh
@@ -51,8 +51,3 @@ then
     docker run --runtime=nvidia --rm nvidia/cuda:9.0-base nvidia-smi
 
 fi
-
-# Docker nvidia troubleshooting
-# sudo systemctl stop docker
-# sudo chmod 666 /var/run/docker.sock
-# sudo dockerd --add-runtime=nvidia=/usr/bin/nvidia-container-runtime
